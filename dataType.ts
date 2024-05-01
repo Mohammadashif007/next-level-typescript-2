fb95b6117ed86a8ed0699ee5ef83ae24a8bca760
Mohammad Ashif <Mohammad Ashif>
2024-05-01 20:47:57 +0600

    typescript type

---
+++ dataType.ts
@@ -0,0 +1,20 @@
+// ! Basic data type
+
+// ! number
+const num:number = 555
+
+// ! string
+const str:string = "Mohammad"
+
+// ! boolean 
+const bool:boolean = false;
+
+// ! undefined
+const und: undefined = undefined;
+
+// ! null
+const n:null = null;
+
+
+
+
