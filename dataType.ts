8504919c66725b4b3c5548d93a343a7270e52b64
Mohammad Ashif <Mohammad Ashif>
2024-05-01 20:59:32 +0600

    learned different types number, boolean, string, tuple

--- dataType.ts
+++ dataType.ts
@@ -1,20 +1,25 @@
 // ! Basic data type
 
 // ! number
-const num:number = 555
+const num: number = 555;
 
 // ! string
-const str:string = "Mohammad"
+const str: string = "Mohammad";
 
-// ! boolean 
-const bool:boolean = false;
+// ! boolean
+const bool: boolean = false;
 
 // ! undefined
 const und: undefined = undefined;
 
 // ! null
-const n:null = null;
+const n: null = null;
+
+let friends: string[] = ["Mohammad", "Ashif"];
+let rollNumber: number[] = [1, 2, 3, 4, 5];
 
 
+// tuple
 
-
+let coordinates:[number, number] = [44, 56];
+let nameAgeMarried:[string, number, boolean] = ["Biden", 87, true];
